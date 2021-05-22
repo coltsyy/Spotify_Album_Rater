edfb182009895defe4bf200a58c096f707667856
Ethan Coulter <coltsyy@gmail.com>
2021-05-21 21:45:21 -0400

    Beginning of layout to webpage

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask;
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/sugon')
-def hello():
-    return 'Garglon deez'
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
