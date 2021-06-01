683b64b98dcf5fefd8f8792464f70f5f173ed8c5
JamesSommervilleLaurier <81589112+JamesSommervilleLaurier@users.noreply.github.com>
2021-05-31 22:20:09 -0400

    created setup file

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-#Hello hello ok ok
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'flask',
+    'spotipy',
+    'html5lib',
+    'requests',
+    'requests_html',
+    'beautifulsoup4',
+    'pathlib',
+    'pandas'
+]
+
+setup(
+    name='SpotifyAlbumRater',
+    version='1.0',
+    description='Application for rating spotify songs',
+    author='James Sommerville ,Ethan Coulter',
+    author_email='Jsommerville99@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires
+)
